bba6a9a74357adfabc41ec384728a744ff12c18a
Leonel Nusdeo <leonel.nusdeo@gmail.com>
2024-02-20 14:21:46 -0300

    Express settings chapter.

--- index.js
+++ index.js
@@ -3,8 +3,19 @@ const morgan = require('morgan');
 
 const app = express();
 
+// Settings
+app.set('case sensitive routing', true);
+app.set('appName', 'Express Course');
+app.set('port', 4000);
+
+// Middlewares
 app.use(express.json());
 app.use(morgan('dev'));
+
+// Routes
+app.get('/username', (req, res) => {
+	res.send('Username route')
+})
 
 app.get('/profile', (req, res) => {
 	console.log(req.body);
@@ -18,17 +29,17 @@ app.all('/about', (req, res) => {
 // El orden IMPORTA. Si este middleware estuviera arriba de todo
 // en todas las rutas se validaria login
 // Asi se requiere solo en /dashboard pero no en /profile ni /about
-app.use((req, res, next) => {
-	if (req.query.login === 'leonel@mail.com') {
-		next()
-	} else {
-		res.send('No autorizado')
-	}
-})
+// app.use((req, res, next) => {
+// 	if (req.query.login === 'leonel@mail.com') {
+// 		next()
+// 	} else {
+// 		res.send('No autorizado')
+// 	}
+// })
 
 app.get('/dashboard', (req, res) => {
 	res.send('Dashboard page')
 })
 
-app.listen(3000);
-console.log(`Server on port ${3000}`);
+app.listen(app.get('port'));
+console.log(`Server ${app.get('appName')} on port ${app.get('port')}`);
